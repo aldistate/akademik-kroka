c99c641734a4977569a6dacfbd62517c92e0b7a5
aldistate <aldistate@yahoo.com>
2023-04-06 23:23:32 +0700

    UI improvements

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.index')
+@section('title', 'Dashboard')
+
+@section('main')
+  <div class="row">
+    <div class="col-md-12 col-sm-12 ">
+      <div class="dashboard_graph">
+        
+      </div>
+    </div>
+  </div>
+@endsection
